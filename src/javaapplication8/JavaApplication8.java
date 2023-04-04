a72506a742e2f0e3ba0dd6578856aefde8501634
emiliano <emisoria12345@gmail.com>
2023-04-04 12:18:58 -0300

    modificacion

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -4,13 +4,21 @@ package javaapplication8;
 
 public class JavaApplication8 {
 
-    int numero1 = 1;
-    int numero2 = 2;
+    
        
     public static void main(String[] args) {
+        int num1= 15;
+        int num2 = 15;
+        if (num2 > num1)
+            System.out.println("num2 es mayor que num1");
+        else if(num1 == num2)
+              System.out.println("son iguales");
+        else
+              System.out.println("num1 es mayor a num2");
+        else 
+        System.out.println("hola");
 
     }
-          
-    
+            
 }
 
