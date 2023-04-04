6a84ff85cb1bc3ce993a55228ed691dfee1f61fe
emiliano <emisoria12345@gmail.com>
2023-04-04 12:08:40 -0300

    first commit

---
+++ src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,16 @@
+
+package javaapplication8;
+
+
+public class JavaApplication8 {
+
+    int numero1 = 1;
+    int numero2 = 2;
+       
+    public static void main(String[] args) {
+
+    }
+          
+    
+}
+
